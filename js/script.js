87f34529fa6c1c24419d4e4f38769d2b16ee7b36
PJLobetti31 <99275095+PJLobetti31@users.noreply.github.com>
2022-03-24 14:48:33 +0000

    edited it to work

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // Copyright (c) 2022 PJ Lobetti All rights reserved
 //
 // Created by: PJ Lobetti
-// Created on: Mar 2022
+// Created on: Sep 2022
 // This file contains the JS functions for index.html
 
 function enterClicked () {
   // this function gets user's first name and age and displays it back to user
 
-  // get street name from text field
-  let Street Name = document.getElementById("street-name").value
-	// get door number from text field and cast it to integer
-  let Number = parseInt(document.getElementById("number-entered").value)
+  // get first name from text field
+  let firstName = document.getElementById("first-name").value
+	// get age from text field and cast it to integer
+  let userAge = parseInt(document.getElementById("age-entered").value)
 
-  // display street and door number back to user
-  document.getElementById('user-info').innerHTML = "Your street name is " + Street Name + " and your door number is " + Number "."
+  // display name and age back to user
+  document.getElementById('user-info').innerHTML = "Your street name is " + firstName + " and your street number is " + userAge + " ."
 }
