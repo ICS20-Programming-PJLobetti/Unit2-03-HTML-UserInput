8d17d4f807121884907b04ec604ed7fa5e7a2c89
PJLobetti31 <99275095+PJLobetti31@users.noreply.github.com>
2022-03-24 02:03:05 +0000

    I tired my best but I can't get my variable to work

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@
 function enterClicked () {
   // this function gets user's first name and age and displays it back to user
 
-  // get first name from text field
-  let firstName = document.getElementById("first-name").value
-	// get age from text field and cast it to integer
-  let userAge = parseInt(document.getElementById("age-entered").value)
+  // get street name from text field
+  let Street Name = document.getElementById("street-name").value
+	// get door number from text field and cast it to integer
+  let Number = parseInt(document.getElementById("number-entered").value)
 
-  // display name and age back to user
-  document.getElementById('user-info').innerHTML = "Your first name is " + firstName + " and you are " + userAge + " years old."
+  // display street and door number back to user
+  document.getElementById('user-info').innerHTML = "Your street name is " + Street Name + " and your door number is " + Number "."
 }
